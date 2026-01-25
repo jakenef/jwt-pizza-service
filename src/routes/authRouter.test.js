777d0b6072bce3daaf91a6a96b411191c48951c5
jakenef <jakenef@byu.edu>
2026-01-25 15:34:16 -0700

    feat: add ESLint configuration and linting script
    - Added ESLint as a dev dependency and created a configuration file for linting JavaScript code.
    - Updated package.json to include a lint script.
    - Created a CI workflow to run linting on push to the main branch.
    - Enhanced test suite with a logout test case for the authentication route.

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,5 +1,10 @@
 const request = require("supertest");
 const app = require("../service");
+// const { Role, DB } = require("../database/database.js");
+
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+  jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
 
 const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
 let testUserAuthToken;
@@ -21,8 +26,31 @@ test("login", async () => {
   expect(loginRes.body.user).toMatchObject(expectedUser);
 });
 
+test("logout", async () => {
+  const loginRes = await request(app).put("/api/auth").send(testUser);
+  const token = loginRes.body.token;
+  const logoutRes = await request(app)
+    .delete("/api/auth")
+    .set("Authorization", `Bearer ${token}`);
+
+  expect(logoutRes.status).toBe(200);
+});
+
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(
     /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/,
   );
 }
+
+// function randomName() {
+//   return Math.random().toString(36).substring(2, 12);
+// }
+
+// async function createAdminUser() {
+//   let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
+//   user.name = randomName();
+//   user.email = user.name + "@admin.com";
+
+//   user = await DB.addUser(user);
+//   return { ...user, password: "toomanysecrets" };
+// }
